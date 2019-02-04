a5f6d47aca6e9608b81bee9eaa6cc52d82518a8d
Feolius <feolius@gmail.com>
2019-02-05 00:26:50 +0700

    Added gapi authentication.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
+/* global gapi chrome */
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import SearchInput from './components/SearchInput.js';
 
 class App extends Component {
-  render() {
-    return (
-        <SearchInput/>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {token: false};
+    }
+
+    componentDidMount() {
+        gapi.load('client', {
+            callback: () => {
+                chrome.identity.getAuthToken({'interactive': true}, (token) => {
+                    gapi.load('client', {
+                        callback: () => {
+                            gapi.client.setToken({access_token: token});
+                            this.setState({token: token});
+                        },
+                        onerror: () => {
+                            // @TODO handle error here properly
+                            alert('gapi.client failed to load!');
+                        },
+                        timeout: 5000, // 5 seconds.
+                        ontimeout: () => {
+                            // @TODO handle error here properly
+                            alert('gapi.client could not load in a timely manner!');
+                        }
+                    });
+                });
+            },
+            onerror: () => {
+                // @TODO handle error here properly
+                alert('gapi.client failed to load!');
+            },
+            timeout: 5000, // 5 seconds.
+            ontimeout: () => {
+                // @TODO handle error here properly
+                alert('gapi.client could not load in a timely manner!');
+            }
+        });
+    }
+
+    render() {
+        let app =
+            <div className="loader-screen">
+                <span className="glyphicon glyphicon-refresh spinner"></span>
+            </div>;
+        if (this.state.token) {
+          app = <SearchInput/>;
+        }
+        return app;
+    }
 }
 
 export default App;
